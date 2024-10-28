ede75000c051058639493ea208c91266fc132304
kassem2004 <kazeinab123@gmail.com>
2024-10-28 18:53:55 +0300

    Implemented cd

--- shell.cpp
+++ shell.cpp
@@ -42,29 +42,62 @@ void parseString(std::string &inputArgs, std::vector<std::string> &parsedArgs){
     }
 }
 
-int processInput(std::string &inputArgs, std::vector<std::string> &parsedArgs, std::vector<std::string> &parsedArgsPiped){
+int isPiped(std::vector<std::string> &parsedArgs){
+    for (std::string &arg : parsedArgs) { 
+        if(!arg.compare("|")){
+            return 1;
+        };
+    }
+    return 0;
+}
+
+int processInput(std::string &inputArgs, std::vector<std::string> &parsedArgs){
     //determines if built-in, system, or piped
     parseString(inputArgs, parsedArgs);
-    
-    return 0;
+    if(!parsedArgs[0].compare("cd") || !parsedArgs[0].compare("help") || !parsedArgs[0].compare("exit")){
+        return 0;
+    } else if (isPiped(parsedArgs)){
+        return 2;
+    } 
+    return 1;
+}
+
+void execBuiltIn(std::vector<std::string> &parsedArgs){
+    if(!parsedArgs[0].compare("cd")){
+        if(parsedArgs.size() > 2){
+            std::cerr << "cd: too many arguments\n";
+        } else {
+            if (chdir(parsedArgs[1].c_str()) != 0) {
+                perror("cd");
+            }
+        }
+    }
 }
 
 int main(){
     std::string inputArgs;
-    std::vector<std::string> parsedArgs, parsedArgsPiped;
+    std::vector<std::string> parsedArgs;
     int exec_flag = 0;
     init_shell();
 
     while(1){
         printDir();
         if (takeUserInput(inputArgs)){
-            std::cout << inputArgs << "\n"; //testing
-            exec_flag = processInput(inputArgs, parsedArgs, parsedArgsPiped);
-            for (std::string &arg : parsedArgs) { //testing
+            //std::cout << inputArgs << "\n"; //testing
+            exec_flag = processInput(inputArgs, parsedArgs);
+            /*for (std::string &arg : parsedArgs) { //testing
                 std::cout << arg << "\n";
+            }*/
+
+            if(exec_flag == 0){
+                execBuiltIn(parsedArgs);
+            } else if (exec_flag == 1){
+                //execSys(parsedArgs);
+            } else {
+                //execPiped(parsedArgs);
             }
+
             parsedArgs.clear();
-            parsedArgsPiped.clear();
         }
     }
 
