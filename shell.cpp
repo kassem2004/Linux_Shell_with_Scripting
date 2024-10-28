5fec94951adadcbd9874e5c272aca373cc969ca0
kassem2004 <kazeinab123@gmail.com>
2024-10-28 16:40:00 +0300

    Implemented parsing

--- shell.cpp
+++ shell.cpp
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <sstream>
+#include <vector>
 
 #define MAX_LETTERS 100
 #define MAX_COMMS 100
-#define clear() printf("\033[H\033[J")
 
 void init_shell() {
-    clear();
     std::cout << "\n******************************************";
     std::cout << "\n*****************MY SHELL*****************";
     std::cout << "\n******************************************";
@@ -23,26 +23,48 @@ void printDir(){
     std::cout << "Dir: " << cwd;
 }
 
-int takeUserInput(char *inputArgs){
-    char input[MAX_LETTERS];
+int takeUserInput(std::string &inputArgs){
     std::cout << "\n>>>"; 
-    std::cin.getline(input, MAX_LETTERS);
-    if(strlen(input) != 0){
-        strcpy(inputArgs, input);
-        return 0;
+    std::getline(std::cin, inputArgs);
+    if (!inputArgs.empty()) {
+        return 1;
     }
-    return 1;
+    return 0;
+}
+
+void parseString(std::string &inputArgs, std::vector<std::string> &parsedArgs){
+    char delimeter = ' ';
+
+    std::istringstream stream(inputArgs);
+    std::string token;
+    while(getline(stream, token, delimeter)){
+        parsedArgs.push_back(token);
+    }
+}
+
+int processInput(std::string &inputArgs, std::vector<std::string> &parsedArgs, std::vector<std::string> &parsedArgsPiped){
+    //determines if built-in, system, or piped
+    parseString(inputArgs, parsedArgs);
+    
+    return 0;
 }
 
 int main(){
-    char inputArgs[MAX_LETTERS], *parsedArgs[MAX_COMMS], *parsedArgsPiped[MAX_COMMS];
+    std::string inputArgs;
+    std::vector<std::string> parsedArgs, parsedArgsPiped;
     int exec_flag = 0;
     init_shell();
 
     while(1){
         printDir();
         if (takeUserInput(inputArgs)){
-            std::cout << "Testing!";
+            std::cout << inputArgs << "\n"; //testing
+            exec_flag = processInput(inputArgs, parsedArgs, parsedArgsPiped);
+            for (std::string &arg : parsedArgs) { //testing
+                std::cout << arg << "\n";
+            }
+            parsedArgs.clear();
+            parsedArgsPiped.clear();
         }
     }
 
